11bfadd1bcaf5f6c514af834775051997559f98d
Modi <Prabhat>
2022-03-19 05:12:58 +0530

    my first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+public static void main(String[]args)
+    {
+int x =5;
+int y =6;
+int z=x+y;
+System.out.println("the value of z="+z);
+
+
+
+
+     }
+
+
+
+
+
+}
---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+public static void main (String[]args) 
+{
+String[]Fruits={"mango","orange","papaya","lemon"};
+System.out.println(Fruits[2]);
+         }
+              }
